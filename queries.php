3e5c21342992a52daca030629cc73602a18e7a78
Alexander Georgiev <a.georgiev.91@gmail.com>
2023-11-13 18:18:18 +0200

    Update queries.php

--- queries.php
+++ queries.php
@@ -64,3 +64,27 @@ function test_posts_results($posts, \WP_Query $query)
 	return $posts;
 }
 add_filter('posts_results', 'test_posts_results', 10, 2);
+
+function redirect_b2b_to_custom_homepage($query) {
+    if ($query->is_main_query() && is_user_logged_in()) {
+        //work-around for using is_front_page() in pre_get_posts
+        //known bug in WP tracked by https://core.trac.wordpress.org/ticket/21790
+        $front_page_id = get_option('page_on_front');
+        $current_page_id = $query->get('page_id');
+        $is_static_front_page = 'page' == get_option('show_on_front');
+
+        if ($is_static_front_page && $front_page_id == $current_page_id) {
+            $current_user = wp_get_current_user();
+            $user = new WP_User($current_user->ID);
+            // Check if the user has a role containing the substring 'haendler'
+            $haendler_roles = array_filter($current_user->roles, function($role) {
+                return strpos($role, 'haendler') !== false;
+            });
+
+            if (!empty($haendler_roles)) {
+                $query->set('page_id', '92112');
+            }
+        }
+    }
+}
+add_action('pre_get_posts', 'redirect_b2b_to_custom_homepage');
